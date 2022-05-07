35f0d51ccf789d4e7ae1306bc9500db1c23ce5b9
shokes <oyatioshoke@yahoo.com>
2022-05-07 16:03:26 +0100

    implementation of smooth scrolling

--- src/GoToTop.js
+++ src/GoToTop.js
@@ -4,7 +4,10 @@ import { useLocation } from 'react-router-dom';
 const GoToTop = function () {
   const routePath = useLocation();
   const onTop = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   };
 
   useEffect(() => {
